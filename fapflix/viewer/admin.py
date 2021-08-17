9f36726cbb6f5cded6a6413895278dbce9a0597a
einaeffchen <github@dummer.dev>
2021-08-17 16:14:56 +0200

    Register actors in admin panel

--- fapflix/viewer/admin.py
+++ fapflix/viewer/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Videos, Labels
+from .models import Actors, Videos, Labels
 
 # Register your models here.
 admin.site.register(Labels)
+admin.site.register(Actors)
 admin.site.register(Videos)
