3eee2d41bd7568e6383d004bf3226033b6a6126e
einaeffchen <github@dummer.dev>
2021-08-17 16:14:48 +0200

    Add path for media uploads

--- fapflix/fapflix/urls.py
+++ fapflix/fapflix/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import debug_toolbar
 
 urlpatterns = [
@@ -22,3 +24,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
