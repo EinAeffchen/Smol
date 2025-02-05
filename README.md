2ee58c4647973818bb3c124f70a92b71e3b7f5c0
einaeffchen <github@dummer.dev>
2025-02-05 13:15:14 +0900

    Update requirements and README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Small Media Organization System
 
+This is a very rudimentary setup only meant for local hosting.
+
 ## Contribute
 
 - Fork and create a new branch, give it an appropriate name and submit a pull request.
@@ -9,16 +11,14 @@ Small Media Organization System
 
 ## Get started
 
-* TODO
-
 ### Setup
-* TODO
+* Rename Makefile.template to Makefile
+* Run `pip install -r requirements.txt`
+* Either link your local files to smol/local_media or change the `MEDIA_ROOT` in [settings.py](smol/smol/settings.py).
+* Run first time analysis using `make analyze`
+* Start server with `make up`
+* Open browser: [Server](http://localhost:8080)
 
-### Setable Environment Variables
-* *HOST* - Allowed Host/Address
-
-### First Use
-* TODO
 
 ## Licensed under: 
 [GNU AFFERO GENERAL PUBLIC LICENSE](./LICENSE.md)
--- smol/viewer/management/commands/analyze_all.py
+++ smol/viewer/management/commands/analyze_all.py
@@ -1,22 +1,16 @@
 from django.core.management.base import BaseCommand
 from viewer.models import Video
-from viewer.video_processor import recognize_faces
+from viewer.video_processor import recognize_faces, add_labels_by_path
 from tqdm import tqdm
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for video in tqdm(Video.objects.all()):
-            print("Analyzing ", video.id)
+            add_labels_by_path(video)
             video.face_encodings
         print("Finished creating encodings! Starting Recognition")
-        # for video in tqdm(Video.objects.all()):
-        #     if (
-        #         len(video.related_videos.all()) <= 1
-        #         and not video.ran_recognition
-        #     ):
-        #         print("Recognizing ", video.filename)
-        #         recognize_faces(video)
-        #     else:
-        #         print("Video already got related videos. Skipping!")
-        #     video.ran_recognition = True
+        for video in tqdm(Video.objects.all()):
+            if not video.ran_recognition:
+                recognize_faces(video)
+            video.ran_recognition = True
